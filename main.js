a37ed4b39a82467fb26645f20eb3c2fec5764479
EduardoBorraz <49501291+EduardoBorraz@users.noreply.github.com>
2020-09-08 20:20:35 -0500

    EduardoBorraz
    Diseño de las cards

---
+++ main.js
@@ -0,0 +1,17 @@
+//1) Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro),Ej: mifuncion("Salas") devolverá true.
+
+///2) Programa una función que reciba un número y evalúe si es capicúa o no(que se lee igual en un sentido que en otro),Ej: miFuncion(2002) devolverá true.
+
+//3) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), Ej. miFuncion(5) devolverá 120.
+
+//4) Programa una función que determine si un número es primo (aquel que solo es divisible por sí mismo y 1) o no, Eje: miFuncion(7) devolverá true.
+
+//5) Programa una función que determine si un número es par o impar, Ej: miFuncion(29) devolverá Impar
+
+//6) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, Ej: miFuncion(0,"C") devolverá 32°F.
+
+//7) Programa una función que devuelva el monto final después de aplicar un descuento a una cantidad dada, pe. miFuncion(1000, 20) devolverá 800.
+
+//8) Programa una función que dada una fecha válida determine cuantos años han pasado hasta el día de hoy, pe. miFuncion(new Date(1984,4,23)) devolverá 35 años (en 2020).
+
+//9) Programa una función que dada una cadena de texto cuente el número de vocales y consonantes, pe. miFuncion("Hola Mundo") devuelva Vocales: 4, Consonantes: 5.
